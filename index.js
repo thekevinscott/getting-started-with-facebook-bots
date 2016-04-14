8d6acc483240839d6fbfa824b734c958ea0afa18
Kevin Scott <kevin@scottlabsllc.com>
2016-04-14 15:50:50 -0400

    Add POST call response from facebook

--- index.js
+++ index.js
@@ -16,4 +16,18 @@ app.get('/webhook/', function (req, res) {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
-})
+});
+
+// respond to post calls from facebook
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
