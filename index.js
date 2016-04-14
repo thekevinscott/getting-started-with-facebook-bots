e6668a256ace70ee326a4f32002e80576c875f68
Kevin Scott <kevin@scottlabsllc.com>
2016-04-14 16:28:46 -0400

    Update body parser to remove deprecated options

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello world');
