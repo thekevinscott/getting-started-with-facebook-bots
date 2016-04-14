2722459b050c9649edf65f903e15fc2933790d91
Kevin Scott <kevin@scottlabsllc.com>
2016-04-14 15:41:06 -0400

    Add code to respond to Facebook's verification challenge

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var token = '<YOUR TOKEN HERE>';
 var express = require('express');
 var app = express();
 
@@ -8,3 +9,11 @@ app.get('/', function (req, res) {
 app.listen(5000, function () {
   console.log('Listening on port 5000');
 });
+
+// respond to facebook's verification
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === token) {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
